13348ab607750bb111296cd17fd0675c5bd271db
AndresLex <andrescamargocisf@gmailcom>
2023-01-18 18:49:28 -0500

    Segundo Cambio

--- app-prueba-angular/src/app/app.component.ts
+++ app-prueba-angular/src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+type Movies = {
+  name: string,
+  available: number,
+  disponible: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-prueba-angular';
+  title = 'Peliculas';
+  movies: Movies[] = [
+    {
+      name: "One Piece",
+      available: 15,
+      disponible: 0
+    },
+    {
+      name: "Naruto",
+      available: 13,
+      disponible: 0
+    }
+  ];
+
+  addDisponible (nameMovie: string) {
+    const movieIndex = this.movies.findIndex(
+      movie => movie.name === nameMovie
+    )
+
+    this.movies[movieIndex].disponible += 1;
+  }
+
+  removeDisponible (nameMovie: string) {
+    const movieIndex = this.movies.findIndex(
+      movie => movie.name === nameMovie
+    )
+
+    this.movies[movieIndex].disponible -= 1;
+  }
 }
